197f245291659f846ff211434258a9f3d336c7db
revina aureliya juliani <revinawrites@gmail.com>
2026-04-22 13:56:42 +0700

    after counter pengunjung

--- counter_pengunjung.php
+++ counter_pengunjung.php
@@ -7,12 +7,24 @@ class Pengunjung {
     public function __construct() {
         self::$jumlah++;
     }
+
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
+$p4 = new Pengunjung();
+$p5 = new Pengunjung();
 
-echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+echo "Jumlah Pengunjung Sebelum Reset: " . Pengunjung::$jumlah;
+
+echo "<br><br>";
+
+Pengunjung::reset();
+
+echo "Jumlah Pengunjung Setelah Reset: " . Pengunjung::$jumlah;
 
 ?>
